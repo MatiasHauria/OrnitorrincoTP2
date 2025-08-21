bbf36028f07739ed82ff35034e836fc97036e6dd
dannitacode <dannavalenchu076@gmail.com>
2025-08-21 14:24:23 -0300

    Creacion de la clase OrniVerde

--- src/ornitorrinco/OrniVerde.java
+++ src/ornitorrinco/OrniVerde.java
@@ -1,5 +1,28 @@
 package ornitorrinco;
 
-public class OrniVerde {
+import java.util.Scanner;
+
+public class OrniVerde extends Castor implements Pata {
+
+    private String nombre;
+
+    public OrniVerde(int cola, int velocidad, String nombre) {
+        super(cola, velocidad);
+        this.nombre = "Blue";
+    }
+
+    @Override
+    public void tocarOrgano() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do-Re-Mi, presione Enter para continuar.");
+        sc.nextLine();
+        System.out.println("Fa-Sol-La-Si");
+    }
     
+    public void tocarGuitorgan() {
+        super.tocarguitarra();
+        this.tocarOrgano();
+        System.out.println("Cuac cuaac...!");
+    }
+
 }
