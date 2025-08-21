8f3a77ac3f02fbcb37254000625327f7c00071c6
MatiasHauria <hauriatute@gmail.com>
2025-08-21 14:06:39 -0300

    Clase ornitorrinco azul agregada

--- src/ornitorrinco/OrniAzul.java
+++ src/ornitorrinco/OrniAzul.java
@@ -1,5 +1,41 @@
 package ornitorrinco;
 
-public class OrniAzul {
+import java.util.Scanner;
+
+public class OrniAzul extends Castor implements Pata{
+    private double propulsion;
+    
+    public OrniAzul(double propulsion, int cola, int velocidad){
+    super(cola, velocidad);
+    this.propulsion = propulsion;
+    }
+
+    public double getPropulsion() {
+        return propulsion;
+    }
+
+    public void setPropulsion(double propulsion) {
+        this.propulsion = propulsion;
+    }
+    
+    
+    @Override
+    public void nadar(){
+        Scanner scan = new Scanner(System.in);
+        propulsion = scan.nextInt();
+        int velocidadfinal = super.velocidad;
+        velocidadfinal += propulsion;
+        System.out.println("El ornitorrinco azul nada a: " + velocidadfinal +" km/s con su propulsion");
+    }
+
+    @Override
+    public void tocarOrgano() {
+        System.out.println("Do-Re-Mi");
+        Scanner enter = new Scanner(System.in);
+        System.out.println("Presione ENTER para seguir tocando las siguientes notas");
+        enter.hasNext();
+        System.out.println("Fa-Sol-La-Si");
+    }
+    
     
 }
